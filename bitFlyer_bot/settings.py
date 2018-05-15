d2e2e536e1e04e8d697f7154ee2ddd2d4febf741
Taisuke Hinata <hnttisk@gmail.com>
2018-05-15 09:12:49 +0900

    Add app

--- bitFlyer_bot/settings.py
+++ bitFlyer_bot/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'bitFlyer_bot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
