44e00ea038b8b188aca3fd0e5d563868c6d9d573
Taisuke Hinata <hnttisk@gmail.com>
2018-05-17 10:02:07 +0900

    Add bootstrap to templates

--- bitFlyer_bot/settings.py
+++ bitFlyer_bot/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',  # django-bootstrap4
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'trading/templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
