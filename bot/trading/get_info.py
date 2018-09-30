e5bdbdc72e27563355b0f6b5d3c72a6c6f00c268
Taisuke Hinata <hnttisk@gmail.com>
2018-09-30 21:35:57 +0900

    Add func to format json for slack

--- bot/trading/get_info.py
+++ bot/trading/get_info.py
@@ -34,9 +34,14 @@ def post_notification(info):
     Post notification to #dev
     """
     api_endpoint = SLACK_URL
+    text = _process_array_for_slack(_process_json_array(USER_NAME, info))
     requests.post(
         api_endpoint,
-        data=json.dumps({"text": str(_process_json_array(USER_NAME, info))})
+        data=json.dumps({
+            "username": "btcfxjp",
+            "text": text,
+            "icon_emoji": ":ghost:",
+        })
     )
 
 
@@ -44,7 +49,7 @@ def _process_json_array(user, json_array):
 
     new_list = []
     for i in json_array:
-        new_dict = {'user': user}
+        new_dict = {'ユーザー': user}
         for k, v in i.items():
             if k == 'size':
                 new_dict['枚数'] = v
@@ -57,9 +62,19 @@ def _process_json_array(user, json_array):
                     jst_time = dt.astimezone(timezone('Asia/Tokyo')).strftime('%Y/%m/%d %H:%M:%S')
                 new_dict['注文時刻'] = jst_time
             elif k == 'side':
-                 new_dict['注文'] = v
+                new_dict['注文'] = v
         new_list.append(new_dict)
     return new_list
+
+
+def _process_array_for_slack(json_array):
+
+    text = ''
+    for json_dict in json_array:
+        for k, v in json_dict.items():
+            text += str(k) + ': ' + str(v) + '\n'
+        text += '\n'
+    return text
 
 
 def build_local_bot():
@@ -73,8 +88,8 @@ def build_local_bot():
         res = get_info('/v1/me/getchildorders?product_code=FX_BTC_JPY&count=1')
         order_time = res[0]['child_order_date']
         if order_time != befor_order_time:
-            print('Ordered')
             post_notification(res)
+            print('Ordered: ' + _process_json_array(USER_NAME, res)[0]['注文時刻'])
         befor_order_time = order_time
 
 # build_local_bot()
--- bot/trading/serializers.py
+++ bot/trading/serializers.py
@@ -5,14 +5,14 @@ class TradingSerializer(serializers.Serializer):
 
     count = serializers.IntegerField(
         required=False,
-        default = 100,        
-        min_value = 1,
-        max_value = 1000
+        default=100,
+        min_value=1,
+        max_value=1000
     )
     before = serializers.CharField(required=False)
     after = serializers.CharField(required=False)
     product_code = serializers.ChoiceField(
         required=False,
-        choices = ['BTC_JPY', 'FX_BTC_JPY', 'ETH_BTC']
+        choices=['BTC_JPY', 'FX_BTC_JPY', 'ETH_BTC']
     )
     from_date = serializers.DateTimeField(required=False)
