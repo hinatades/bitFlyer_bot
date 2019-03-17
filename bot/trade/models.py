2df9a6a3034550feb0ad0f7b381b3604a19c8dc3
Taisuke Hinata <hnttisk@gmail.com>
2019-03-17 19:55:50 +0900

    Make ticks model

--- bot/trade/models.py
+++ bot/trade/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class TICKs(models.Model):
+
+    timestamp = models.DateField()
+    product_code = models.CharField(max_length=50)
+    tick_id = models.CharField(max_length=50)
+    best_ask = models.CharField(max_length=50)
+    best_ask_size = models.CharField(max_length=50)
+    best_bid = models.CharField(max_length=50)
+    best_bid_size = models.CharField(max_length=50)
+    total_ask_depth = models.CharField(max_length=50)
+    total_bid_depth = models.CharField(max_length=50)
+    ltp = models.CharField(max_length=50)
+    volume = models.SmallIntegerField()
+    volume_by_product = models.CharField(max_length=50)
